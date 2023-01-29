922d0d72413900cfab0fb3e725ee4227e1791fc7
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2023-01-29 23:38:00 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,8 @@
 <template>
   <section class="md:h-64 lg:h-80 xl:h-96 2xl:h-128 grid place-content-center">
     <div class="text-center">
-      <div v-if="!user" class="grid">
-        <h1 class="text-2xl md:text-5xl">Hello World</h1>
-        <NuxtLink class="py-2 px-6 bg-indigo-600 hover:bg-indigo-700 rounded-md text-white no-underline duration-150" :to="{name: 'dashboard'}">Dashboard</NuxtLink>
-      </div>
-      <h1 v-else class="text-2xl md:text-5xl">Hello World</h1>
+      <h1 class="text-2xl md:text-5xl">Hello World</h1>
+      <NuxtLink v-if="!user" class="py-2 px-6 bg-indigo-600 hover:bg-indigo-700 rounded-md text-white no-underline duration-150" :to="{name: 'dashboard'}">Dashboard</NuxtLink>
     </div>
   </section>
 </template>
