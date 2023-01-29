3dcb14625ec472eb548aacf4914b8b1b790a35b2
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2023-01-29 20:03:00 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="md:h-64 lg:h-80 xl:h-96 2xl:h-128 grid place-content-center">
     <div class="text-center">
-      <h1 class="text-2xl md:text-5xl">Nuxt 3 / Content 2</h1>
+      <h1 class="text-2xl md:text-5xl">Hello World</h1>
 
       <NuxtLink class="py-2 px-6 bg-indigo-600 hover:bg-indigo-700 rounded-md text-white no-underline duration-150" :to="{name: 'dashboard'}">Dashboard</NuxtLink>
     </div>
